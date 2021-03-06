5bd5f8ae9f3cef730085123eb79c44f63e8566c9
mariele <mariele_rbr@hotmail.com>
2021-03-06 01:39:57 -0300

    Array

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const person = {
 }
 
 console.log(`${person.name} tem ${person.age} anos`)
+
+
+// ARRAY 
+
+const animals = [
+  'lion',
+  'Monkey',
+  'Cat'
+]
+
+// acessar valores dentro do Array
+console.log(animals[1]) // => acessar o animal na posição 1
+console.log(animals.length) // => saber quantas posições tem dentro do Array
