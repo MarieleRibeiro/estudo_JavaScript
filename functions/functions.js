429c603957a9b079f3d10670ea61d1f14e668eca
mariele <mariele_rbr@hotmail.com>
2021-03-11 00:01:43 -0300

    Function scope

--- functions/functions.js
+++ functions/functions.js
@@ -1,29 +1,11 @@
-// function expression
-// function anonymous 
+// FUNCTION SCOPE
 
-const sum = function (number1, number2) { // parametros da função( parameters)
-  let total = number1 + number2
-  return total
+let subject
+
+function createThink() {
+  subject = 'study'
 }
 
-/* EXEMPLO 1 */
-//sum(2, 3) // executar a função/ argumentos - aguments
-//sum(4, 5)
-//sum(56, 3009845)
-
-
-/* EXEMPLO 2 */
-
-let number1 = 34
-let number2 = 25
-
-console.log(`o numero 1 é ${number1}`)
-console.log(`o numero 2 é ${number2}`)
-console.log(`a soma é ${sum(number1, number2)}`)
-
-function fazerSuco(fruta1, fruta2) {
-  return fruta1 + fruta2
-}
-const copo = fazerSuco('banana', 'maça')
-
-console.log(copo);
+console.log(subject)
+createThink()
+console.log(subject)
